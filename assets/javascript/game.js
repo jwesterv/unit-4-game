c22d4ace115a441c11a355d1361ea585be8e313c
James Westervelt <james.a.westervelt@gmail.com>
2018-11-25 18:45:02 -0800

    Threw away bootstrap and wrote simpler html/css elements and started jQuery game elements

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Ready code when loaded
+$(document).ready(function() {
+
+//Number from clicking crystals
+var yourNumber = 0;
+
+//Starting variables including crystals
+var wins = 0;
+var losses = 0;
+var crystals;
+
+//Random Target Number we want to reach
+var randomNumber = randomNumberGenerator();
+
+}
