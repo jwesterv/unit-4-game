cd9bb1be82812bbaf68cadcbfba94fb572327322
James Westervelt <james.a.westervelt@gmail.com>
2019-04-23 15:11:03 -0700

    added more logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,54 @@
 // Ready code when loaded
-$(document).ready(function() {
+$(document).ready(function () {
 
-//Number from clicking crystals
-var yourNumber = 0;
+    //Number from clicking crystals
+    var yourNumber = 0;
 
-//Starting variables including crystals
-var wins = 0;
-var losses = 0;
-var crystals;
+    //Starting variables needed for building
 
-//Random Target Number we want to reach
-var randomNumber = randomNumberGenerator();
+    var wins = 0;
+
+    var losses = 0;
+
+    var score = 0;
+
+    var yellowVal = 0;
+
+    var blueVal = 0;
+
+    var greenVal = 0;
+
+    var redVal = 0;
+
+    var randomNum = '';
+
+    var minNum = 3;
+
+    var maxNum = 10;
+
+
+
+    //Random Target Number we want to reach
+
+    function randomTargetNumber() {
+        return Math.floor(math.random() * 40) + 1)
+}
+
+//Giving our crystals a random number value
+
+function crystalValue() {
+
+    var min = 1;
+    var max = 7;
+
+    randomNum = randomNumberRange(minNum, maxNum);
+    // Random yellow value
+    yellowVal = randomNumberRange(min, max);
+    // Random blue value
+    blueVal = randomNumberRange(min, max);
+    // Random green value
+    greenVal = randomNumberRange(min, max);
+    // Random red value
+    redVal = randomNumberRange(min, max);
 
 }
